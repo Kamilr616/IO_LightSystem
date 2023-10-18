35b1ece2876491b717f1d0842a14727aa7285408
Kamil <kamilr616@gmail.com>
2023-10-18 15:53:13 +0200

    Initial commit

---
+++ src/app.py
@@ -0,0 +1,8 @@
+# app.py
+
+__author__ = "Kamil Rataj"
+__credits__ = ["Mateusz Ciszek", "Natalia Martemianow"]
+__version__ = "0.0.1"
+__maintainer__ = "Kamil Rataj"
+__status__ = "Development"
+
---
+++ src/main.py
@@ -0,0 +1,22 @@
+# main.py
+
+__author__ = "Kamil Rataj"
+__credits__ = ["Mateusz Ciszek", "Natalia Martemianow"]
+__version__ = "0.0.1"
+__maintainer__ = "Kamil Rataj"
+__status__ = "Development"
+
+# import cv2
+# import numpy as np
+# import mediapipe as mp
+# import math
+# import os
+# from app import *
+
+
+def main():
+    print('Inżynieria oprogramowania')
+
+
+if __name__ == '__main__':
+    main()

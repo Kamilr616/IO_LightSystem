b89a2ddf79e48c8d1198ff7cf2f12456eddfe134
Kamil <kamilr616@gmail.com>
2023-11-08 17:31:22 +0100

    tracker class

--- src/app.py
+++ src/app.py
@@ -6,43 +6,5 @@ __version__ = "0.0.1"
 __maintainer__ = "Kamil Rataj"
 __status__ = "Development"
 
-import mediapipe as mp
-import cv2
-
-
-class HandTracker:
-    def __init__(self, mode=False, max_hands=1, detection_con=0.5, model_complexity=1, track_con=0.5):
-        # Inicjalizacja parametrów śledzenia dłoni
-        self.mode = mode
-        self.max_hands = max_hands
-        self.detection_con = detection_con
-        self.model_complexity = model_complexity
-        self.track_con = track_con
-        # Inicjalizacja modułów MediaPipe
-        self.mp_hands = mp.solutions.hands
-        self.hands = self.mp_hands.Hands(self.mode, self.max_hands, self.model_complexity,
-                                         self.detection_con, self.track_con)
-        self.mp_draw = mp.solutions.drawing_utils
-
-    def find_hands(self, image, draw=True):
-        # Przetwarzanie obrazu w poszukiwaniu dłoni
-        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        self.results = self.hands.process(image_rgb)
-
-        if self.results.multi_hand_landmarks:
-            for hand_landmarks in self.results.multi_hand_landmarks:
-                if draw:
-                    # Rysowanie punktów i połączeń dłoni
-                    self.mp_draw.draw_landmarks(image, hand_landmarks, self.mp_hands.HAND_CONNECTIONS)
-        return image
-
-    def find_positions(self, image, hand_no=0):
-        # Znajdowanie pozycji (landmarków) dłoni
-        lm_list = []
-        if self.results.multi_hand_landmarks:
-            hand = self.results.multi_hand_landmarks[hand_no]
-            for id, lm in enumerate(hand.landmark):
-                h, w, c = image.shape
-                cx, cy = int(lm.x * w), int(lm.y * h)
-                lm_list.append([id, cx, cy])
-        return lm_list
+class app:
+    pass

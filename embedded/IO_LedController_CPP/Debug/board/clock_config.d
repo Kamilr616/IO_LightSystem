e36bed6025c48d886319f1b94d937d09fa78cfd5
Kamil <kamilr616@gmail.com>
2023-11-14 15:02:37 +0100

    embedded files added

---
+++ embedded/IO_LedController_CPP/Debug/board/clock_config.d
@@ -0,0 +1,33 @@
+board/clock_config.o board/clock_config.d: ../board/clock_config.c \
+ /home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/drivers/fsl_power.h \
+ /home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/drivers/fsl_common.h \
+ /home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/device/fsl_device_registers.h \
+ /home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/device/LPC804.h \
+ /home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/CMSIS/core_cm0plus.h \
+ /home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/CMSIS/cmsis_version.h \
+ /home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/CMSIS/cmsis_compiler.h \
+ /home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/CMSIS/cmsis_gcc.h \
+ /home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/device/system_LPC804.h \
+ /home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/device/LPC804_features.h \
+ /home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/drivers/fsl_common_arm.h \
+ /home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/drivers/fsl_clock.h \
+ /home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/drivers/fsl_reset.h \
+ /home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/drivers/fsl_clock.h \
+ ../board/clock_config.h \
+ /home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/drivers/fsl_common.h
+/home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/drivers/fsl_power.h:
+/home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/drivers/fsl_common.h:
+/home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/device/fsl_device_registers.h:
+/home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/device/LPC804.h:
+/home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/CMSIS/core_cm0plus.h:
+/home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/CMSIS/cmsis_version.h:
+/home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/CMSIS/cmsis_compiler.h:
+/home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/CMSIS/cmsis_gcc.h:
+/home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/device/system_LPC804.h:
+/home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/device/LPC804_features.h:
+/home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/drivers/fsl_common_arm.h:
+/home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/drivers/fsl_clock.h:
+/home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/drivers/fsl_reset.h:
+/home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/drivers/fsl_clock.h:
+../board/clock_config.h:
+/home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/drivers/fsl_common.h:
---
+++ embedded/IO_LedController_CPP/Debug/drivers/fsl_usart.d
@@ -0,0 +1,25 @@
+drivers/fsl_usart.o drivers/fsl_usart.d: ../drivers/fsl_usart.c \
+ ../drivers/fsl_usart.h ../drivers/fsl_common.h \
+ /home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/device/fsl_device_registers.h \
+ /home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/device/LPC804.h \
+ /home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/CMSIS/core_cm0plus.h \
+ /home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/CMSIS/cmsis_version.h \
+ /home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/CMSIS/cmsis_compiler.h \
+ /home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/CMSIS/cmsis_gcc.h \
+ /home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/device/system_LPC804.h \
+ /home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/device/LPC804_features.h \
+ ../drivers/fsl_common_arm.h ../drivers/fsl_clock.h \
+ ../drivers/fsl_reset.h
+../drivers/fsl_usart.h:
+../drivers/fsl_common.h:
+/home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/device/fsl_device_registers.h:
+/home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/device/LPC804.h:
+/home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/CMSIS/core_cm0plus.h:
+/home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/CMSIS/cmsis_version.h:
+/home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/CMSIS/cmsis_compiler.h:
+/home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/CMSIS/cmsis_gcc.h:
+/home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/device/system_LPC804.h:
+/home/kamil/PycharmProjects/IO_LightSystem/embedded/IO_LedController_CPP/device/LPC804_features.h:
+../drivers/fsl_common_arm.h:
+../drivers/fsl_clock.h:
+../drivers/fsl_reset.h:
